38731b22bbb9eaef8dac7784991800bfc7193e7e
Metrow <mcmove95@gmail.com>
2025-12-17 13:53:23 +0800

    Upgrade Options UI update

--- Scripts/character_body_2d.gd
+++ Scripts/character_body_2d.gd
@@ -8,6 +8,19 @@ const MOVE_SPEED := 40.0
 var health: int = MAX_HEALTH
 var is_alive := true
 
+#Canvas Layer
+@onready var expBar = get_node("%ExperienceBar")
+@onready var lblLevel = get_node("%lbl_level")
+@onready var levelPanel = get_node("%LevelUp")
+@onready var upgradeOptions = get_node("%UpgradeOptions")
+@onready var itemOptions = preload("res://ItemAssets/")
+@onready var sndLevelUp = get_node("%snd_levelup")
+@onready var healthBar = get_node("%HealthBar")
+@onready var lblTimer = get_node("%lblTimer")
+@onready var collectedWeapons = get_node("%CollectedWeapons")
+@onready var collectedUpgrades = get_node("%CollectedUpgrades")
+@onready var someScript = preload("res://Scripts/some_script.gd")
+
 
 func _physics_process(_delta: float) -> void:
 	var input_vector := Vector2(
@@ -46,3 +59,28 @@ func die() -> void:
 
 func _on_hurtbox_area_entered(_area) -> void:
 	take_damage(1)
+	
+func levelup() -> void:
+	sndLevelUp.play()
+	lblLevel.text = "Level: %s" % experience_level
+
+	var tween = levelPanel.create_tween()
+	tween.tween_property(
+		levelPanel,
+		"position",
+		Vector2(220, 50),
+		0.2
+	).set_trans(Tween.TRANS_QUINT).set_ease(Tween.EASE_IN)
+
+	levelPanel.visible = true
+
+	var options := 0
+	var optionsmax := 3
+
+	while options < optionsmax:
+		var option_choice = itemOptions.instantiate()
+		option_choice.item = get_random_item()
+		upgradeOptions.add_child(option_choice)
+		options += 1
+
+	get_tree().paused = true
