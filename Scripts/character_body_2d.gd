96e3d5e23c94d30499fe2c72af09fbc80772e6b9
Mart Barbon <martbarbon8@gmail.com>
2025-12-18 18:16:50 +0800

    Update character_body_2d.gd

--- Scripts/character_body_2d.gd
+++ Scripts/character_body_2d.gd
@@ -18,10 +18,10 @@ var collected_experience = 0
 # UI References
 @onready var expBar = get_node("%ExperienceBar")
 @onready var lblLevel = get_node("%lbl_level")
-@onready var levelPanel = get_node("%LevelUp")
-@onready var upgradeOptions = get_node("%UpgradeOptions")
-@onready var itemOptions = preload("res://Scenes/item_option.tscn")
-@onready var healthbar = get_node("%HealthBar")
+@onready var levelPanel = get_node_or_null("%LevelUp")
+@onready var upgradeOptions = get_node_or_null("%UpgradeOptions")
+@onready var sndLevelUp = get_node_or_null("%snd_levelup")
+@onready var healthbar = get_node_or_null("%HealthBar")
 @onready var lblTimer = get_node_or_null("%lblTimer")
 @onready var collectedWeapons = get_node_or_null("%CollectedWeapons")
 @onready var collectedUpgrades = get_node_or_null("%CollectedUpgrades")
@@ -44,12 +44,6 @@ func spawn_sword():
 	if not sword_weapon:
 		sword_weapon = sword_scene.instantiate()
 		add_child(sword_weapon)
-		healthbar.init_health(MAX_HEALTH)  # Initializes max and current
-		healthbar.health = health           # <-- Use the property, not update_health()
-	else:
-		push_warning("HealthBar node not found! Health UI will not update.")
-	set_expbar(experience, calculate_experiencecap())
-	
 
 func _physics_process(_delta: float) -> void:
 	if not is_alive:
@@ -68,7 +62,7 @@ func take_damage(amount: int) -> void:
 		return
 	health = clamp(health - amount, 0, MAX_HEALTH)
 	if healthbar:
-		healthbar.health = health  # <-- Use property
+		healthbar.health = health
 	if health == 0:
 		die()
 
@@ -113,27 +107,6 @@ func check_levelup() -> void:
 		experience_level += 1
 		exp_cap = calculate_experiencecap()
 
-		calculate_experience(gem_exp)
-
-
-func calculate_experience(gem_exp):
-	var exp_required = calculate_experiencecap()
-	collected_experience += gem_exp
-
-	if experience + collected_experience > exp_required:
-		collected_experience -= exp_required - experience
-		experience_level += 1
-		lblLevel.text = str("Level: ",experience_level)
-		experience = 0
-		exp_required = calculate_experiencecap()
-		levelup()
-	else:
-		experience += collected_experience
-		collected_experience = 0
-
-	set_expbar(experience, exp_required)
-
-
 func calculate_experiencecap():
 	var exp_cap = experience_level
 	if experience_level < 20:
@@ -142,39 +115,9 @@ func calculate_experiencecap():
 		exp_cap = 95 * (experience_level - 19) * 8
 	else:
 		exp_cap = 255 + (experience_level - 39) * 12
-
 	return exp_cap
 
 func set_expbar(set_value = 1, set_max_value = 100):
 	if expBar:
 		expBar.value = set_value
 		expBar.max_value = set_max_value
-	expBar.value = set_value
-	expBar.max_value = set_max_value
-
-func levelup():
-	lblLevel.text = str("Level: ", experience_level)
-	var tween = levelPanel.create_tween()
-	tween.tween_property(levelPanel, "position", Vector2(913, 28), 0.2)\
-		.set_trans(Tween.TRANS_QUINT)\
-		.set_ease(Tween.EASE_IN)
-	tween.play()
-	levelPanel.visible = true
-	var options = 0
-	var optionsmax = 3
-	while options < optionsmax:
-		var option_choice = itemOptions.instantiate()
-		upgradeOptions.add_child(option_choice)
-		options += 1
-	get_tree().paused = true
-
-	
-func upgrade_character(upgrade):
-	var option_children = upgradeOptions.get_children()
-	for i in option_children: 
-		i.queue_free()
-	levelPanel.visible = false
-	levelPanel.position = Vector2(1181,29)
-	get_tree().paused = false
-	calculate_experience(0)
-	
