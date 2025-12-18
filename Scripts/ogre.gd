faf0e3826fa82fce39c470147fe29aa913450421
MoromasaRen <146726008+RenMoromasa@users.noreply.github.com>
2025-12-18 21:32:12 +0800

    added

--- Scripts/ogre.gd
+++ Scripts/ogre.gd
@@ -10,5 +10,18 @@ func _ready():
 func _physics_process(delta):
 	if is_dead:
 		return
-	
-	super._physics_process(delta)
+		
+	if player:
+		var direction = global_position.direction_to(player.global_position)
+		velocity = direction * speed
+		move_and_slide()
+		
+		# Flip sprite
+		if direction.x < 0:
+			animated_sprite.flip_h = true
+		elif direction.x > 0:
+			animated_sprite.flip_h = false
+		
+		# Play run animation
+		if animated_sprite.sprite_frames.has_animation("Run"):
+			animated_sprite.play("Run")
