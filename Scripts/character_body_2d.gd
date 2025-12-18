fa4bb612603308caf9377f6f0a818d80cdcd15e6
Metrow <mcmove95@gmail.com>
2025-12-18 18:27:57 +0800

    script update

--- Scripts/character_body_2d.gd
+++ Scripts/character_body_2d.gd
@@ -18,13 +18,14 @@ var collected_experience = 0
 # UI References
 @onready var expBar = get_node("%ExperienceBar")
 @onready var lblLevel = get_node("%lbl_level")
-@onready var levelPanel = get_node_or_null("%LevelUp")
-@onready var upgradeOptions = get_node_or_null("%UpgradeOptions")
-@onready var sndLevelUp = get_node_or_null("%snd_levelup")
-@onready var healthbar = get_node_or_null("%HealthBar")
+@onready var levelPanel = get_node("%LevelUp")
+@onready var upgradeOptions = get_node("%UpgradeOptions")
+@onready var itemOptions = preload("res://Scenes/item_option.tscn")
+@onready var healthbar = get_node("%HealthBar")
 @onready var lblTimer = get_node_or_null("%lblTimer")
 @onready var collectedWeapons = get_node_or_null("%CollectedWeapons")
 @onready var collectedUpgrades = get_node_or_null("%CollectedUpgrades")
+
 
 # Weapon
 var sword_scene = preload("res://Scenes/sword_weapon.tscn")
@@ -93,19 +94,25 @@ func _on_grab_area_area_entered(area: Area2D) -> void:
 func _on_collect_area_area_entered(area: Area2D) -> void:
 	if area.is_in_group("loot"):
 		var gem_exp = area.collect()
-		gain_experience(gem_exp)
+		calculate_experience(gem_exp)
 
-func gain_experience(amount: int) -> void:
-	current_exp += amount
-	check_levelup()
-	set_expbar(current_exp, calculate_experiencecap())
+func calculate_experience(gem_exp):
+	var exp_required = calculate_experiencecap()
+	collected_experience += gem_exp
 
-func check_levelup() -> void:
-	var exp_cap = calculate_experiencecap()
-	while current_exp >= exp_cap:
-		current_exp -= exp_cap
+	if experience + collected_experience > exp_required:
+		collected_experience -= exp_required - experience
 		experience_level += 1
-		exp_cap = calculate_experiencecap()
+		lblLevel.text = str("Level: ",experience_level)
+		experience = 0
+		exp_required = calculate_experiencecap()
+		levelup()
+	else:
+		experience += collected_experience
+		collected_experience = 0
+
+	set_expbar(experience, exp_required)
+
 
 func calculate_experiencecap():
 	var exp_cap = experience_level
@@ -115,9 +122,36 @@ func calculate_experiencecap():
 		exp_cap = 95 * (experience_level - 19) * 8
 	else:
 		exp_cap = 255 + (experience_level - 39) * 12
+
 	return exp_cap
 
+
 func set_expbar(set_value = 1, set_max_value = 100):
-	if expBar:
-		expBar.value = set_value
-		expBar.max_value = set_max_value
+	expBar.value = set_value
+	expBar.max_value = set_max_value
+
+func levelup():
+	lblLevel.text = str("Level: ", experience_level)
+	var tween = levelPanel.create_tween()
+	tween.tween_property(levelPanel, "position", Vector2(913, 28), 0.2)\
+		.set_trans(Tween.TRANS_QUINT)\
+		.set_ease(Tween.EASE_IN)
+	tween.play()
+	levelPanel.visible = true
+	var options = 0
+	var optionsmax = 3
+	while options < optionsmax:
+		var option_choice = itemOptions.instantiate()
+		upgradeOptions.add_child(option_choice)
+		options += 1
+	get_tree().paused = true
+
+	
+func upgrade_character(upgrade):
+	var option_children = upgradeOptions.get_children()
+	for i in option_children: 
+		i.queue_free()
+	levelPanel.visible = false
+	levelPanel.position = Vector2(1181,29)
+	get_tree().paused = false
+	calculate_experience(0)
